7915e068bb580fbe937a9704a17e8249ca188ae7
b2devtech <110045855+b2devtech@users.noreply.github.com>
2023-05-21 02:49:03 +0000

    commit for b2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import logo from '/assets/b2coin.png';
+import wallet from '/assets/wallet.png';
+import linkedin from '/assets/174857.png';
+import site from '/assets/6471842.png';
+import 'react-toastify/dist/ReactToastify.css'; // import first
+import { ToastContainer, toast } from 'react-toastify'; // then this
+
+
 
 export default function App() {
 
-  const wave = () => {
-    
-  }
-  
-  return (
-    <div className="mainContainer">
+  const notify = () => toast("Conecte-se a carteira Metamask instalada!");
 
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        notify
+        //console.log("Conecte-se a carteira Metamask instalada!");
+        return;
+      } else {
+        alert("Temos o objeto ethereum", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        alert("Encontrada a conta autorizada:", account);
+        setCurrentAccount(account)
+      } else {
+        alert("Nenhuma conta autorizada foi encontrada")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+  * Implemente aqui o seu método connectWallet
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("MetaMask encontrada!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      alert("Conectado na carteira: " + accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Recuperado o número de tchauzinhos...", count.toNumber());
+      } else {
+        console.log("Objeto Ethereum não encontrado!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+  return (
+    <div className="mainContainer" onLoad={notify}>
+         <nav>
+          <ul className="list">
+              <li className="items">
+               <a href="https://www.linkedin.com/in/geraldovianajr/"     target="_blank" rel="noopener noreferrer">
+                 <img src={linkedin}  width="30px"/>
+      </a>
+              </li>
+              <li className="items">
+                               <a href="https://b2dev.com.br/"     target="_blank" rel="noopener noreferrer">
+                 <img src={site}  width="30px"/>
+      </a>
+                
+              </li>
+            </ul>
+          <button className="btn">BTN</button>
+        </nav>
       <div className="dataContainer">
-        <div className="header">
-        👋 Olá Pessoal!
+        <center>
+        <img src={logo} alt="Logo" width="200px" /> 
+        </center>
+          <div className="header">
+         B2COIN - Projeto de estudos Web3-Cripto
         </div>
 
         <div className="bio">
-        Eu sou o danicuki e já trabalhei com música, sabia? Legal, né? Conecte sua carteira  Ethereum wallet e me manda um tchauzinho!
+       
         </div>
-
-        <button className="waveButton" onClick={wave}>
-          Mandar Tchauzinho 🌟
+        <button  className="snip1564"  onClick={notify}>
+          Fazer Transação 
         </button>
+         
+        {/*
+        * Se não existir currentAccount, apresente este botão
+        */}
+        {!currentAccount && (
+          <button className="snip1564"    onClick={connectWallet}>
+            <img src={wallet}  width="30px" />  Conectar carteira
+          </button>
+     
+        )}
       </div>
+      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import 'react-toastify/dist/ReactToastify.css'; // import first
+import { ToastContainer, toast } from 'react-toastify'; // then this
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+
