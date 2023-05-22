4aa516f056228587f9d8dddf6ba7a40eda2df891
b2devtech <110045855+b2devtech@users.noreply.github.com>
2023-05-22 19:23:15 +0000

    Transações funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,74 @@ import linkedin from '/assets/174857.png';
 import site from '/assets/6471842.png';
 import 'react-toastify/dist/ReactToastify.css'; // import first
 import { ToastContainer, toast } from 'react-toastify'; // then this
+import abi from './utils/WavePortal.json';
 
 
 
 export default function App() {
+ 
 
-  const notify = () => toast("Conecte-se a carteira Metamask instalada!");
+     /*
+   * Método para consultar todos os tchauzinhos do contrato
+   */
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
+        /*
+         * Chama o método getAllWaves do seu contrato inteligente
+         */
+        const waves = await wavePortalContract.getAllWaves();
+        const waveTxn = await wavePortalContract.wave("esta é uma mensagem");
+
+        /*
+         * Apenas precisamos do endereço, data/horário, e mensagem na nossa tela, então vamos selecioná-los
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Armazenando os dados
+         */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Objeto Ethereum não existe!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+ 
   const [currentAccount, setCurrentAccount] = useState("");
+
+   const contractAddress = "0xfB11EB37bB2F2660D53fA8D55D1D3E3F7e9964D3";
+   const contractABI = abi.abi;
+ 
+ const [allWaves, setAllWaves] = useState([]);
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
 
       if (!ethereum) {
-        notify
+        
+        toast.success('Conecte-se a carteira Metamask');
         //console.log("Conecte-se a carteira Metamask instalada!");
         return;
       } else {
        toast.success('Blockchain Ethereum reconhecida');
+       
         
       }
 
@@ -34,7 +83,8 @@ export default function App() {
       if (accounts.length !== 0) {
         const account = accounts[0];
          toast.success('Encontrada a conta autorizada:' + account);
-        setCurrentAccount(account)
+        setCurrentAccount(account);
+         getAllWaves();
       } else {
         toast.error('Nenhuma conta autorizada foi encontrada');
       }
@@ -58,6 +108,7 @@ export default function App() {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
       toast.success("Conectado na carteira: " + accounts[0]);
       setCurrentAccount(accounts[0]);
+
     } catch (error) {
       console.log(error)
     }
@@ -67,7 +118,10 @@ export default function App() {
     checkIfWalletIsConnected();
   }, [])
 
+     
+
 const wave = async () => {
+  
     try {
       const { ethereum } = window;
 
@@ -75,16 +129,31 @@ const wave = async () => {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
+      
+      
         let count = await wavePortalContract.getTotalWaves();
         console.log("Recuperado o número de tchauzinhos...", count.toNumber());
+        
+        /*
+        * Executar o tchauzinho a partir do contrato inteligente
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Minerando...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Minerado -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Total de tchauzinhos recuperado...", count.toNumber());
+        getAllWaves();
       } else {
         console.log("Objeto Ethereum não encontrado!");
       }
     } catch (error) {
       console.log(error)
     }
-}
+  }
+
 
   return (
     <div className="mainContainer" >
@@ -115,7 +184,7 @@ const wave = async () => {
         <div className="bio">
        
         </div>
-        <button  className="snip1564"  onClick={notify}>
+        <button  className="snip1564"  onClick={wave}>
           Fazer Transação 
         </button>
          
@@ -123,13 +192,22 @@ const wave = async () => {
         * Se não existir currentAccount, apresente este botão
         */}
         {!currentAccount && (
-          <button onLoad={notify} className="snip1564"    onClick={connectWallet}>
+          <button className="snip1564"    onClick={connectWallet}>
             <img src={wallet}  width="30px" />  Conectar carteira
           </button>
      
         )}
+
+  {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Endereço: {wave.address}</div>
+              <div>Data/Horário: {wave.timestamp.toString()}</div>
+              <div>Mensagem: {wave.message}</div>
+            </div>)
+        })}
+        
       </div>
-      
     </div>
   );
 }
